b6c54c852f6c6decb70c0a1698ff874bcdba0778
AnonUser1035 <rbohlul1@jh.edu>
2024-05-11 18:37:59 -0400

    made a test program for stimulus make it callable

--- Most recent code 4-29/ssh_test.py
+++ Most recent code 4-29/ssh_test.py
@@ -1,5 +1,6 @@
 import paramiko
 import stimuli
+from time import sleep
 
 host = "192.168.0.5"
 username = "smlab-picar-dev"
@@ -7,14 +8,18 @@ password = "2mysore1lab"
 
 client = paramiko.client.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(host, username=username, password=password)
+#client.connect(host, username=username, password=password)
 
 def run_and_print(command):
     _stdin, _stdout, _stderr = client.exec_command(command)
     print(_stdout.read().decode())
     print(_stderr.read().decode())
 
-for i in range(1):
-    run_and_print("cd Desktop//picar-dev//new_categorization//; python new_categorization.py")
+for i in range(10):
+    #run_and_print("cd Desktop//picar-dev//new_categorization//; python new_categorization.py")
+    stimuli.display_stimulus()
+    print("stimulus shown")
+    sleep(1)
+
 
 client.close()
